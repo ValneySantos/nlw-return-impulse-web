c444d267ba6d9475a36df68b74a9b97b0f7b8048
Valney <filhovalney@gmail.com>
2022-05-03 10:07:41 -0300

    finalizando WidgetButton e criando a pasta assets

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -1,7 +1,10 @@
 import { Popover } from '@headlessui/react'
+import { X } from 'phosphor-react'
 
 export function CloseButton(){
   return (
-    <Popover></Popover>
+    <Popover.Button className="top-5 right-5 absolute text-zinc-400 hover:text-zinc-100" title="Fechar formulário Button">
+        <X weight="bold" className="w-4 h-4 "/>
+    </Popover.Button>
   )
 }
